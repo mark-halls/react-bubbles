98f02ac03bb70d327bc2f2dffaa730b4cc45bef1
Mark Halls <mark.halls@gmail.com>
2019-11-15 10:12:09 -0700

    Add PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
+
 import "./styles.scss";
 
 function App() {
+  const [appState, setAppState] = useState({ loggedIn: false });
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
+        <Switch>
+          <PrivateRoute isAuthenticated={appState.loggedIn} path="/bubblepage">
+            <BubblePage />
+          </PrivateRoute>
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/" component={Login} />
+          {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        </Switch>
       </div>
     </Router>
   );
---
+++ client/src/components/PrivateRoute/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        rest.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;

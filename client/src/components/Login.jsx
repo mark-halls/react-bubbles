0215856754938e927737e24639bbaf45051dccc0
Mark Halls <mark.halls@gmail.com>
2019-11-15 10:51:13 -0700

    Add missing state dependencies, remove unused vars

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = props => {
         props.history.push("/bubblepage");
       })
       .catch(err => console.error(err));
-  }, [form]);
+  }, [form, props.history]);
 
   return (
     <div>

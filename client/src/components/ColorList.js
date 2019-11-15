4f71c4157748d30f625b10670f941c6231516961
Mark Halls <mark.halls@gmail.com>
2019-11-15 11:13:11 -0700

    Can add new color

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -9,6 +9,7 @@ const initialColor = {
 const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -44,12 +45,22 @@ const ColorList = ({ colors, updateColors }) => {
       .catch(err => console.error(err));
   };
 
+  const handleAdd = () => {
+    axiosAuth()
+      .post(`http://localhost:5000/api/colors`, colorToAdd)
+      .then(res => {
+        updateColors(res.data);
+        setColorToAdd(initialColor);
+      })
+      .catch(err => console.error(err));
+  };
+
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
         {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
+          <li key={color.id} onClick={() => editColor(color)}>
             <span>
               <span
                 className="delete"
@@ -99,8 +110,42 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
+      {/* <div className="spacer" /> */}
       {/* stretch - build another form here to add a color */}
+      <form>
+        <legend>add a color</legend>
+        <label>
+          color name:
+          <input
+            onChange={e =>
+              setColorToAdd({
+                ...colorToAdd,
+                color: e.target.value,
+                id: colors.length
+              })
+            }
+            value={colorToAdd.color}
+          />
+        </label>
+        <label>
+          hex code:
+          <input
+            onChange={e =>
+              setColorToAdd({
+                ...colorToAdd,
+                code: { hex: e.target.value },
+                id: colors.length
+              })
+            }
+            value={colorToAdd.code.hex}
+          />
+        </label>
+        <div className="button-row">
+          <button type="button" onClick={handleAdd}>
+            Add Color
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
